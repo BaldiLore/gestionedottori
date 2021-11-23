de494b46a973e6c48187f280ec4051ca9807301d
BaldiLore <baldilorenzo2600@gmail.com>
2021-11-23 10:23:13 +0100

    Commit creazione repository

---
+++ src/main/java/it/prova/gestionedottori/repository/DottoreRepository.java
@@ -0,0 +1,10 @@
+package it.prova.gestionedottori.repository;
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import it.prova.gestionedottori.model.Dottore;
+
+public interface DottoreRepository extends PagingAndSortingRepository<Dottore, Long>, JpaSpecificationExecutor<Dottore>{
+
+}
